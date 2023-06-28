e8bcaaa7729ec49898642ace6824107cadc7fede
BalGu <52451260+Bal-Gu@users.noreply.github.com>
2023-06-28 18:01:16 +0200

    basic agent function + outline of indicators

---
+++ Agent.py
@@ -0,0 +1,50 @@
+import random
+
+
+class Agent():
+    def __init__(self, actions):
+        self.fitting = 0
+        self.priceHistory = []
+        self.last_action = 0  # only -1 or 1
+        self.possibleActions = random.choices([True, False], k=actions)
+        self.actionsBeforeAct = 0
+        for i in range(actions):
+            if self.possibleActions[i]:
+                self.generate_action(i)
+
+    def get_score(self) -> float:
+        return self.fitting
+
+    def remove_history(self):
+        self.priceHistory = []
+        self.last_action = 0
+
+    def determine_action(self) -> int:
+        # either return -1 sell;0 do nothing;+1 buy
+        return 1
+
+    def generate_action(self, i):
+        # 0     moving average
+        # 1     exponential moving average
+        # 2     Volume moving average
+        # 3     Average volume line
+        # 4     SAR
+        # 5     moving average cross
+        # 6     exponential moving average cross
+        # 7     Volume moving average cross
+        # 8     Average volume line cross
+        # 9     MACD
+        # 10    RSI
+        # 11    Stochastic RSI
+        # 12    Volume diff
+        # 13    Bollinger Bands
+        # 14    Moving flow index
+        # 15    KDJ index
+        # 16    On balance volume
+        # 17    Commodity Channel Index
+        # 18    Wm%R
+        # 19    Directional Movement Index
+        # 20    MTM - Momentum Index
+        # 21    Ease of Movement Value
+        if i == 0:
+            pass
