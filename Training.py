c7f1244c3745801b1b05665c1454e3ee78064c7a
BalGu <52451260+Bal-Gu@users.noreply.github.com>
2023-06-28 15:14:25 +0200

    general classes

---
+++ Training.py
@@ -0,0 +1,16 @@
+
+class GeneticAlgorythm:
+    def save(self):
+        pass
+
+    def tournament_selection(self):
+        pass
+
+    def mutation(self):
+        pass
+
+    def crossover(self):
+        pass
+
+    def fitness(self):
+        pass
