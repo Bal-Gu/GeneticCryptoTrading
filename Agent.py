49986081b391c216308ef2574751727b92c15927
BalGu <guillaume@ballinger.lu>
2023-07-04 01:35:40 +0200

    cleaning a bit the code

--- Agent.py
+++ Agent.py
@@ -102,79 +102,69 @@ class Agent:
         return self.config[index]["timeframe"]
 
     def movingaverage(self):
-        if self.actionsBeforeAct("0") < len(self.candleHistory):
+        if self.actions_before_act("0") < len(self.candleHistory):
             return 0
             # moving average
         previous_ma = self.config["0"]["previous"]
-        if previous_ma == 0:
-            return 0
+
         ma = 0
-        candle_type = self.selectCandle(self.config["0"]["candle"])
-        candles = self.formatCandles(self.candleHistory, candle_type)
+        candle_type = self.select_candle(self.config["0"]["candle"])
+        candles = self.format_candles(self.candleHistory, candle_type)
         for value in candles:
             ma += value
         current_ma = ma / len(candles)
         self.config["0"]["previous"] = current_ma
+        if current_ma == 0:
+            return 0
         if current_ma >= previous_ma:
             return 1
         else:
             return -1
-    def movingaverageexponential(self):
-        if self.actionsBeforeAct("1") < len(self.candleHistory):
+    def moving_average_exponential(self):
+        if self.actions_before_act("1") < len(self.candleHistory):
             return 0
-        for index in self.config:
-            if not self.config[index]["activate"]:
-                continue
-            # moving average
-            if index == "0":
-                previous_ema = self.config[index]["previous"]
-                if previous_ema == 0:
-                    return 0
-                ema = 0
-                candleType = self.selectCandle(self.config[index]["candle"])
-                candles = self.formatCandles(self.candleHistory, candleType)
-                first = True
-                N = self.config[index]["length"]
-                for value in candles:
-                    if first:
-                        prev = value
-                        first = False
-                        continue
+        previous_ema = self.config["1"]["previous"]
+        ema = 0
+        candle_type = self.select_candle(self.config["1"]["candle"])
+        candles = self.format_candles(self.candleHistory, candle_type)
+        N = self.config["1"]["length"]
+        for value in candles:
+            k = 2 / (N+1)
+            ema = (value - ema) * k + ema
 
-                    k = 2 / (N+1)
-                    ema += value
-
-                current_ema = ema / len(candles)
-                self.config[index]["previous"] = current_ema
-                if current_ema >= previous_ema:
-                    return 1
-                else:
-                    return -1
-    def actionsBeforeAct(self, index):
+        current_ema = ema
+        self.config["1"]["previous"] = current_ema
+        if current_ema == 0:
+            return 0
+        if current_ema >= previous_ema:
+            return 1
+        else:
+            return -1
+    def actions_before_act(self, index):
         if index == "0" or index == "1":
             return self.config[index]["length"] * self.config[index]["timeframe"] >= len(self.candleHistory)
         if index == "2" and len(self.candleHistory) >= 2:
             return True
         return False
 
-    def selectCandle(self, index):
-        candleType = self.config[index]["candle"]
-        if str(candleType).isdigit():
-            return candleType
-        elif candleType == "High":
+    def select_candle(self, index):
+        candle_type = self.config[index]["candle"]
+        if str(candle_type).isdigit():
+            return candle_type
+        elif candle_type == "High":
             return 2
-        elif candleType == "Low":
+        elif candle_type == "Low":
             return 3
-        elif candleType == "Close":
+        elif candle_type == "Close":
             return 4
-        elif candleType == "Open":
+        elif candle_type == "Open":
             return 1
 
-    def formatCandles(candleHistory, candleType, candleTimeframe):
+    def format_candles( self,candleType, candleTimeframe):
         candles = []
         window = []
 
-        for candle in candleHistory:
+        for candle in self.candleHistory:
             window.append(candle)
 
             if len(window) == candleTimeframe:
